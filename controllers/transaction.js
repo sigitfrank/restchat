7cd81c07843ffe975a2493936a28edec4097808c
Sigit <sigittt53@gmail.com>
2022-01-20 09:21:43 +0700

    update transaction route

--- controllers/transaction.js
+++ controllers/transaction.js
@@ -11,10 +11,14 @@ const getTransactions = (req, res) => {
             const totalSpent = transactions.map(transaction => +transaction.total_spent).reduce((accumulator, item) => {
                 return accumulator + item
             }, 0)
-            connection.query('SELECT count(customer_id) as total, customer_id, transaction_at FROM purchase_transactions WHERE DATE_SUB(NOW(), INTERVAL 30 DAY) <= DATE(transaction_at) AND customer_id = ? GROUP BY DATE(transaction_at), customer_id', [id], function (err, data) {
+            connection.query('SELECT sum(total_spent), customer_id, transaction_at FROM purchase_transactions WHERE DATE_SUB(NOW(), INTERVAL 30 DAY) <= DATE(transaction_at) AND customer_id = ? GROUP BY DATE(transaction_at), customer_id', [id], function (err, data) {
                 if (err) throw err
-                const last30DaysTransactions = data[0].total
-                return res.status(200).json({ success: true, transactions, totalTransaction, totalSpent, last30DaysTransactions })
+                const last30DaysSpentTransactions = data[0].total_spent
+                connection.query('SELECT count(total_spent), customer_id, transaction_at FROM purchase_transactions WHERE DATE_SUB(NOW(), INTERVAL 30 DAY) <= DATE(transaction_at) AND customer_id = ? GROUP BY DATE(transaction_at), customer_id', [id], function (err, data) {
+                    if (err) throw err
+                    const last30DaysTransactions = data[0].total_spent
+                    return res.status(200).json({ success: true, transactions, totalTransaction, totalSpent, last30DaysSpentTransactions, last30DaysTransactions })
+                })
             })
         })
     })
