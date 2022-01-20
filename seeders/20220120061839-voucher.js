b7a99fad4d6a9d91ed34dcf17f0b63adfaa38f29
Sigit <sigittt53@gmail.com>
2022-01-20 15:06:07 +0700

    update column voucher

--- seeders/20220120061839-voucher.js
+++ seeders/20220120061839-voucher.js
@@ -4,7 +4,6 @@ module.exports = {
   async up (queryInterface, Sequelize) {
     const date = new Date()
     return queryInterface.bulkInsert('vouchers', [{
-      status:true,
       last_claimed:null,
       created_at:date,
       updated_at:date
